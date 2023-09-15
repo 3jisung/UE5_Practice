c002c9eddea23573514c906707a2be58af8ea43c
3jisung <3jisung@naver.com>
2023-09-16 01:17:20 +0900

    init upload

---
+++ Source/Test0702/UIEX/InvenItemSlot.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InvenItemSlot.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST0702_API UInvenItemSlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
